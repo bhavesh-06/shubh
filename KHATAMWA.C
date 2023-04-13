0292096e02d286cc28955c1ddca14ccc20411fd9
Ahirrao Bhavesh <130631961+bhavesh-06@users.noreply.github.com>
2023-04-13 10:38:59 +0530

    Add files via upload

---
+++ KHATAMWA.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+typedef int hours;
+hours hrs,h;
+clrscr();
+printf(" enter hours:");
+scanf("%d",&hrs);
+printf("\n minute=%d",hrs*h);
+printf("\n seconds=%d",hrs*h*h);
+getch();
+}
